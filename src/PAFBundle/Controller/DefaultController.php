b7ecb8566ae926594041e9de64266dca9476fd4c
Alexis Pitchen <apitchen@gmail.com>
2017-04-27 16:30:38 +0200

    start controller

--- src/PAFBundle/Controller/DefaultController.php
+++ src/PAFBundle/Controller/DefaultController.php
@@ -4,16 +4,21 @@ namespace PAFBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+
+        $login->setName('Login');
+        $form = $this->createForm(LoginForm::class);
+
+        $form->handleRequest($request);
+
         return $this->render('PAFBundle:Default:index.html.twig');
     }
-
-    publi
 }
