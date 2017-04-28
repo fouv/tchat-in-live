af572e526ae173767656cd3c69fc1e1bc3ca06f3
Alexis Pitchen <apitchen@gmail.com>
2017-04-28 11:09:26 +0200

    add emoji

--- web/js/main.js
+++ web/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    $('.emoji').emoticonize();
+
     $('.card-close').click(function(e) {
         e.preventDefault();
         $('.card-panel.teal.lighten-2').css('display', 'none');
